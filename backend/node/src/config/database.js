259b97db957a5308f382f7a1bdb6fff5e3a45105
mohanvelaga <mohanvelaga1998@gamil.com>
2026-04-07 21:06:20 +0530

    fixed db

--- backend/node/src/config/database.js
+++ backend/node/src/config/database.js
@@ -1,8 +1,6 @@
 const { Sequelize } = require('sequelize')
 require('dotenv').config()
 
-const { Sequelize } = require('sequelize')
-
 const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
